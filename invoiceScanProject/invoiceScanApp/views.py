d7062a01ed150b1d8d8144431dbb386c7f68274b
ZiniMedAmine <ezzinimohamedamine@isima.u-monastir.tn>
2024-02-20 22:21:46 +0100

    One image upload success

--- invoiceScanProject/invoiceScanApp/views.py
+++ invoiceScanProject/invoiceScanApp/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UploadFileForm
 from .models import User
-# Create your views here.
+from django.core.exceptions import ValidationError
 
 def home(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
-        file = request.FILES['file']
-        user = User.objects.create(doc=file)
-        user.save()
-        return HttpResponse("the user with id " + str(user.pk) + " has the file : "+ str(user.doc))
+        if form.is_valid():
+            try:
+                file = form.cleaned_data['file']
+                user = User.objects.create(doc=file)
+                user.save()
+                return HttpResponse("the user with id "+str(user.pk) + "uploaded this image : "+str(user.doc))
+            except ValidationError as e:
+                form.add_error('file', e)  
+        else:
+            pass
     else:
-        form = UploadFileForm();
-    return render(request,"home.html", {'form': form} )
+        form = UploadFileForm()
+    return render(request, "home.html", {'form': form})
