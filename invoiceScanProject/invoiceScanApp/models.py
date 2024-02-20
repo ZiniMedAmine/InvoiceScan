ab554a3839114451f75631e6166422fd6c0d957d
ZiniMedAmine <ezzinimohamedamine@isima.u-monastir.tn>
2024-02-20 22:00:08 +0100

    upload ne9s

--- invoiceScanProject/invoiceScanApp/models.py
+++ invoiceScanProject/invoiceScanApp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 #from django.contrib.auths.models import user
 # Create pytheo models here.
+class User(models.Model):
+    doc=models.FileField(null=True)
--- invoiceScanProject/invoiceScanApp/views.py
+++ invoiceScanProject/invoiceScanApp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UploadFileForm
+from .models import User
 # Create your views here.
 
 def home(request):
-    return render(request,"home.html")
+    if request.method=='POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        file = request.FILES['file']
+        user = User.objects.create(doc=file)
+        user.save()
+        return HttpResponse("the user with id " + str(user.pk) + " has the file : "+ str(user.doc))
+    else:
+        form = UploadFileForm();
+    return render(request,"home.html", {'form': form} )
