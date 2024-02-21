c973c19a3a32efafeacb140bc5bd5c5f2c60558c
ZiniMedAmine <ezzinimohamedamine@isima.u-monastir.tn>
2024-02-21 22:01:51 +0100

    last

--- invoiceScanProject/invoiceScanApp/models.py
+++ invoiceScanProject/invoiceScanApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 #from django.contrib.auths.models import user
 # Create pytheo models here.
-class User(models.Model):
+class Id(models.Model):
     doc=models.FileField(null=True)
--- invoiceScanProject/invoiceScanApp/views.py
+++ invoiceScanProject/invoiceScanApp/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UploadFileForm
-from .models import User
 from django.core.exceptions import ValidationError
+from .models import Id
 
 def home(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             try:
+                # Handle the single file
                 file = form.cleaned_data['file']
-                user = User.objects.create(doc=file)
-                user.save()
-                return HttpResponse("the user with id "+str(user.pk) + "uploaded this image : "+str(user.doc))
+                id = Id.objects.create(doc=file)
+                id.save()
+                response_message = "the image with id " + str(id.pk) + " uploaded this image : " + str(id.doc)
+
+                # Handle multiple files
+                files = request.FILES.getlist('files')
+                for uploaded_file in files:
+                    id = Id.objects.create(doc=uploaded_file)
+                    id.save() #apparament lmochkla fl id model
+                    response_message += "\nthe image with id " + str(id.pk) + " uploaded this image : " + str(id.doc)
+
+                return HttpResponse(response_message)
             except ValidationError as e:
-                form.add_error('file', e)  
-        else:
-            pass
+                form.add_error('file', e)
     else:
         form = UploadFileForm()
+
     return render(request, "home.html", {'form': form})
