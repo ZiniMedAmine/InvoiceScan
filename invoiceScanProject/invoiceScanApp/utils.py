925819c1a6ef325187956829e9ee3089a25e5242
ZiniMedAmine <ezzinimohamedamine@isima.u-monastir.tn>
2024-03-08 16:36:02 +0100

    push2

--- invoiceScanProject/invoiceScanApp/utils.py
+++ invoiceScanProject/invoiceScanApp/utils.py
@@ -9,7 +9,15 @@ def preprocess_image(image_path):
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    return gray
+    # Apply Gaussian blur for smoothing without excessive blurring
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # Apply adaptive thresholding
+    adaptive_thresh = cv2.adaptiveThreshold(
+        blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )
+
+    return adaptive_thresh
 
 def perform_ocr(preprocessed_image, config='--psm 7'):
     """Performs OCR on a preprocessed image."""
